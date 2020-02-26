2ae9714deb4f812c3cf0be7f2317376f1e192df3
Ryan Overton <ryan.overton@outlook.com>
2020-02-26 11:49:02 -0600

    Make tests fail

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.get('/', (req, res) => res.send("Sup World!"));
+app.get('/', (req, res) => res.send("Hello World!"));
 
 export default app;
 
